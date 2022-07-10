2e84b5ff08d0d1ad9a9d566b8fda9ab7efa9f478
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-07-11 02:50:45 +0800

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
 import "./App.css"
-import { Model, OrbitCamera, useAnimation, usePreload, World } from "lingo3d-react"
+import {
+  Model,
+  OrbitCamera,
+  useAnimation,
+  usePreload,
+  World
+} from "lingo3d-react"
 
 const Game = () => {
-  const anim = useAnimation({ from: 180, to: 0, duration: 5000 })
+  const anim = useAnimation({ from: 180, to: 45, duration: 5000 })
 
   return (
     <div
-     className="bg-cover bg-center w-screen h-screen absolute overflow-hidden text-white"
-     style={{ backgroundImage: "url(bg.jpg)" }}
+      className="bg-cover bg-center w-screen h-screen absolute overflow-hidden text-white"
+      style={{ backgroundImage: "url(bg.jpg)" }}
     >
       <div className="absolute left-0 top-0 w-full h-full bg-gradient-to-t from-gray-700 to-black opacity-75" />
       <div className="relative">
-        <div className="text-5xl font-bold text-center" style={{ transform: "scale(2.5) translateY(50%)" }}>
-          Apple<br />Watch<br />Series
+        <div
+          className="text-5xl font-bold text-center"
+          style={{ transform: "scale(2.5) translateY(50%)" }}
+        >
+          Apple
+          <br />
+          Watch
+          <br />
+          Series
         </div>
       </div>
 
       <World color="transparent" defaultLight="studio">
-        <Model src="apple_watch/scene.gltf" y={-50} rotationZ={anim} boxVisible={false} metalnessFactor={1} roughnessFactor={0.5} />
-        <OrbitCamera active z={200} y={80} zoom={1.5} autoRotate enableDamping />
+        <Model
+          src="apple_watch/scene.gltf"
+          rotationZ={anim}
+          rotationX={45}
+          boxVisible={false}
+          metalnessFactor={1}
+          roughnessFactor={0.5}
+          y={-30}
+        />
+        <OrbitCamera active innerZ={150} autoRotate enableDamping />
       </World>
 
       <div className="absolute bottom-0 w-full p-6">
@@ -30,12 +51,15 @@ const Game = () => {
 }
 
 const App = () => {
-  const progress = usePreload([
-    "bg.jpg",
-    "apple_watch/scene.bin",
-    "apple_watch/scene.gltf",
-    "apple_watch/textures/material_baseColor.jpeg"
-  ], "573kb")
+  const progress = usePreload(
+    [
+      "bg.jpg",
+      "apple_watch/scene.bin",
+      "apple_watch/scene.gltf",
+      "apple_watch/textures/material_baseColor.jpeg"
+    ],
+    "573kb"
+  )
 
   if (progress < 100)
     return (
@@ -44,9 +68,7 @@ const App = () => {
       </div>
     )
 
-  return (
-    <Game />
-  )
+  return <Game />
 }
 
 export default App
